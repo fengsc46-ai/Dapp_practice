ead77778b4f3f12f54fe4e67e6153bdf5bb44ddf
feng <fengsc46@gmail.com>
2025-11-18 20:55:56 +0800

    finsh task1

---
+++ task1/block/BlockReadAndWriter.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+	"context"
+	"crypto/ecdsa"
+	"fmt"
+	"log"
+	"math/big"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/ethclient"
+)
+
+func main() {
+	client, err := ethclient.Dial("https://eth-sepolia.g.alchemy.com/v2/36VCcRTF-M8cAMJFumpHi")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	blockNumber := big.NewInt(5671744)
+
+	queryBlock(err, client, blockNumber)
+	dealTransaction(client)
+}
+
+// 查询区块
+func queryBlock(err error, client *ethclient.Client, blockNumber *big.Int) {
+	header, err := client.HeaderByNumber(context.Background(), blockNumber)
+	fmt.Println(header.Number.Uint64())     // 5671744
+	fmt.Println(header.Time)                // 1712798400
+	fmt.Println(header.Difficulty.Uint64()) // 0
+	fmt.Println(header.Hash().Hex())        // 0xae713dea1419ac72b928ebe6ba9915cd4fc1ef125a606f90f5e783c47cb1a4b5
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	block, err := client.BlockByNumber(context.Background(), blockNumber)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(block.Number().Uint64())     // 5671744
+	fmt.Println(block.Time())                // 1712798400
+	fmt.Println(block.Difficulty().Uint64()) // 0
+	fmt.Println(block.Hash().Hex())          // 0xae713dea1419ac72b928ebe6ba9915cd4fc1ef125a606f90f5e783c47cb1a4b5
+	fmt.Println(len(block.Transactions()))   // 70
+	count, err := client.TransactionCount(context.Background(), block.Hash())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(count) // 70
+}
+
+// 发送交易
+func dealTransaction(client *ethclient.Client) {
+	privateKey, err := crypto.HexToECDSA("fad9c8855b740a0b7ed4c221dbad0f33a83a49cad6b3fe8d5817ac83d38b6a19")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	publicKey := privateKey.Public()
+	publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
+	if !ok {
+		log.Fatal("cannot assert type: publicKey is not of type *ecdsa.PublicKey")
+	}
+
+	fromAddress := crypto.PubkeyToAddress(*publicKeyECDSA)
+	nonce, err := client.PendingNonceAt(context.Background(), fromAddress)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	value := big.NewInt(1000000000000000000) // in wei (1 eth)
+	gasLimit := uint64(21000)                // in units
+	gasPrice, err := client.SuggestGasPrice(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	toAddress := common.HexToAddress("0x5d64e34bc67c2cc73da25b7b98f959811bea5851")
+	var data []byte
+	tx := types.NewTransaction(nonce, toAddress, value, gasLimit, gasPrice, data)
+
+	chainID, err := client.NetworkID(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	signedTx, err := types.SignTx(tx, types.NewEIP155Signer(chainID), privateKey)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.SendTransaction(context.Background(), signedTx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("tx sent: %s", signedTx.Hash().Hex())
+	// TODO
+}
